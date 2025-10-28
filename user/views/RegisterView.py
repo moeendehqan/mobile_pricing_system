05f87c01bc1d747af51023657f459b5af45cf8f3
moeendehqan <moeen.dehqan@gmail.com>
2025-10-28 19:37:30 +0330

    fix(views): correct Response import path in multiple view files
    Update import statements to use the proper path 'rest_framework.response.Response' instead of incorrect 'rest_framework.Response' to resolve import errors

--- user/views/RegisterView.py
+++ user/views/RegisterView.py
@@ -1,5 +1,5 @@
 from rest_framework.permissions import IsAuthenticated
-from rest_framework import Response
+from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 from drf_spectacular.utils import extend_schema
