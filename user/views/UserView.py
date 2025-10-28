32e4d96cc0bec13fde1c8daeed225a966179e254
moeendehqan <moeen.dehqan@gmail.com>
2025-10-28 19:48:34 +0330

    refactor(views): import UserInputSerializer in UserView

--- user/views/UserView.py
+++ user/views/UserView.py
@@ -2,7 +2,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from user.serializers import UserSerializer
+from user.serializers import UserSerializer, UserInputSerializer
 from user.models import User
 from drf_spectacular.utils import extend_schema
  
