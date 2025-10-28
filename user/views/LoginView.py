3c7d5cb708209915122f892f6cbe38d05d655f94
moeendehqan <moeen.dehqan@gmail.com>
2025-10-28 19:57:45 +0330

    refactor(views): use get_user_model instead of direct User import
    Using get_user_model() is the recommended approach in Django for better flexibility with custom user models

--- user/views/LoginView.py
+++ user/views/LoginView.py
@@ -6,7 +6,9 @@ from drf_spectacular.utils import extend_schema
 from user.serializers import LoginInputSerializer
 from user.models import Otp
 from rest_framework_simplejwt.tokens import RefreshToken
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 
