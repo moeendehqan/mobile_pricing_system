4c16519f4065777dab8862f0e9312d4408a21d47
moeendehqan <moeen.dehqan@gmail.com>
2025-11-18 22:23:56 +0330

    chore: update S3 storage access credentials
    Rotate AWS S3 access keys for improved security

--- utils/storage.py
+++ utils/storage.py
@@ -3,8 +3,8 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class CustomS3Storage(S3Boto3Storage):
     bucket_name = 'shikala'
     endpoint_url = 'https://minio.shikala.com'
-    access_key = 'M0P0pT0ShSorWYCpePCi'
-    secret_key = 'E0k68nU1A1VYJtVSKI3Eh7VMIPlJ5znmk9ib0LEF'
+    access_key = 'vUZDxCpXfwRL24WTGhTM'
+    secret_key = 'yzPTaa8k5crrAeUmOUejkLnf6hJ3EzNoef3khaF6'
     file_overwrite = True
     default_acl = 'public-read'
     querystring_auth = False
@@ -16,8 +16,8 @@ class MediaStorage(S3Boto3Storage):
     bucket_name = 'shikala'
     location = 'media'  # prefix داخل bucket
     endpoint_url = 'https://minio.shikala.com'
-    access_key = 'M0P0pT0ShSorWYCpePCi'
-    secret_key = 'E0k68nU1A1VYJtVSKI3Eh7VMIPlJ5znmk9ib0LEF'
+    access_key = 'vUZDxCpXfwRL24WTGhTM'
+    secret_key = 'yzPTaa8k5crrAeUmOUejkLnf6hJ3EzNoef3khaF6'
     file_overwrite = False
     default_acl = 'public-read'
     querystring_auth = False
