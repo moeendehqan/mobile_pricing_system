019a7ffbfda19b88c4f6b72eda58a3fb02d0b4d2
moeendehqan <moeen.dehqan@gmail.com>
2025-12-20 22:09:56 +0330

    feat(accounting): add imports for Response and Sum in views
    Add necessary imports from rest_framework and django.db to support upcoming transaction summary functionality

--- accounting/views.py
+++ accounting/views.py
@@ -3,6 +3,8 @@ from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from .models import transactions
 from .serializers import transactionsSerializer
+from rest_framework.response import Response
+from django.db.models import Sum
 
 
 
