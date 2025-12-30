41fab12301dca3e82d292a931e9041845094ed48
moeendehqan <moeen.dehqan@gmail.com>
2025-12-30 22:07:16 +0330

    fix(zarinpal): disable sandbox mode for production

--- core/services/zarinpal.py
+++ core/services/zarinpal.py
@@ -16,7 +16,7 @@ class ZarinpalService:
     def __init__(self):
         self.merchant_id = "2b894f9c-9b1a-4c32-8a23-2ce5fb66f51a"
         self.access_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiZDcwM2U2YWU1MDYxYjhiMjA2NGRkZmVjNDdkY2FhM2FhNTcwNGIxMzQ4YWE3NGJlM2ZhNjkxMDM0ODYxYTVmNmEzZjAxZDI4YmJlYjQxZjIiLCJpYXQiOjE3NjcxMTIyODMuMTM5Nzg1LCJuYmYiOjE3NjcxMTIyODMuMTM5Nzg3LCJleHAiOjE5MjQ4Nzg2ODMuMTA0MTU2LCJzdWIiOiIzMTQ1MjQzIiwic2NvcGVzIjpbXX0.aoClB6BjoRyV_o0i7lQhug3gkKvj-6ur9VjqNnl9m2No1ljKJ48cRCbyhvFFkdtbmVYuFpzkoStjhr99kB7aQ5R8IEhVII7moSrF8qhnv8am5derJz5fgzYOio2Vp-e4jrS51fJ4ts5Gs-HN8Ewq5eY7wRPVXQk5IFkEI6EhFpDt9qlJ3RLSW41R7eRH77ulIOsM3nLIKjMfSyrddIeVJvehDILlFD2dL15_C3pJQNU5OgGMtIQXbfMWAtXrdOi-DwarX8PR0m0DqZiuqlbshx2nhzCKuDhEJvL17swOoxSnQPsplA4cax3EJKXUMxLATV_e5Crx5XUqseD79Rxij2qMCRTRYdcmrjYJQI6lQztQU_vCIGen2_HVFuHC47H-OoA8udfdwLBFNRdTsG3k-rjH-KNkAP2e-WKx-aN_UExD13NZgxZ5d9-jmi8ak1fpQ2gFX1p9c_u6UlCBLLLR_9qfURhFd9nIEsedl_bxN8mKtuqB5Io27-0biBhbzpVdUv48BqaZw8375ezveKznyRzR6xiqzTAngOWsRHwvU_zhoXXmKeecwyukgd7QDCLWqjRWOmHaUvMGcvUYpNfRNUA2pNZZrEYPebQKX2Jz8IOd44GSe0HTvLF0-oUGC_kanXx9e3iQIir8UOYYjF0oQnRldwfLxb_qbJ-4AsrGKnY"
-        self.sandbox = True
+        self.sandbox = False
         self.callback_url = 'https://api.shikala.com/api/accounting/verify/'
         self.config = Config(
             sandbox=self.sandbox,
